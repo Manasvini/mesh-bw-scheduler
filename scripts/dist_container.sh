1807410e02e2c38c1d42244839df42bed1c4774c
Adwait Bauskar <adwait96bauskar@gmail.com>
2022-10-02 15:22:23 -0400

    Fixup on ssh -t

--- scripts/dist_container.sh
+++ scripts/dist_container.sh
@@ -4,9 +4,9 @@ echo "Creating image on master"
 scp -r ~/mesh/mesh-bw-scheduler/containers/$1 cvuser@cv2:~/mesh/tmp/
 scp -r ~/mesh/mesh-bw-scheduler/scripts/ cvuser@cv2:~/mesh/tmp/scripts/
 
-echo `cat ~/passfile` | ssh cvuser@cv2 -T "bash ~/mesh/tmp/scripts/build_container.sh $1" 
-echo `cat ~/passfile` | ssh cvuser@cv2 -T "sudo ~/mesh/k3s ctr images import ~/mesh/tmp/$1.tar"
-echo `cat ~/passfile` | ssh cvuser@cv2 -T "sudo chmod +777 ~/mesh/tmp/$1.tar"
+echo `cat ~/passfile` | ssh -tt cvuser@cv2 "bash ~/mesh/tmp/scripts/build_container.sh $1" 
+echo `cat ~/passfile` | ssh -tt cvuser@cv2 "sudo ~/mesh/k3s ctr images import ~/mesh/tmp/$1.tar"
+echo `cat ~/passfile` | ssh -tt cvuser@cv2 "sudo chmod +777 ~/mesh/tmp/$1.tar"
 scp cvuser@cv2:~/mesh/tmp/$1.tar ~/mesh/tmp/
 
 echo "Deleting residuals from master"
@@ -18,7 +18,7 @@ for i in 1
 do
     echo "Running script on cv$i"
     scp ~/mesh/tmp/$1.tar cvuser@cv$i:~/mesh/tmp/
-    echo `cat ~/passfile` | ssh cvuser@cv$i -T "sudo ~/mesh/k3s ctr images import ~/mesh/tmp/$1.tar"
+    echo `cat ~/passfile` | ssh -tt cvuser@cv$i "sudo ~/mesh/k3s ctr images import ~/mesh/tmp/$1.tar"
     ssh cvuser@cv$i -T "rm -rf ~/mesh/tmp/$1.tar"
 done
 
