1586255d8588e132d10bda5b412760a3db68b5c6
Adwait Bauskar <adwait96bauskar@gmail.com>
2022-10-02 15:04:26 -0400

    Fixup on sudo over ssh

--- scripts/dist_container.sh
+++ scripts/dist_container.sh
@@ -4,22 +4,22 @@ echo "Creating image on master"
 scp -r ~/mesh/mesh-bw-scheduler/containers/$1 cvuser@cv2:~/mesh/tmp/
 scp -r ~/mesh/mesh-bw-scheduler/scripts/ cvuser@cv2:~/mesh/tmp/scripts/
 
-ssh cvuser@cv2 -t "bash ~/mesh/tmp/scripts/build_container.sh $1" <<< $2
-ssh cvuser@cv2 -t "sudo ~/mesh/k3s ctr images import ~/mesh/tmp/$1.tar" <<< $2
-ssh cvuser@cv2 -t "sudo chmod +777 ~/mesh/tmp/$1.tar" <<< $2
+ssh cvuser@cv2 -t "bash ~/mesh/tmp/scripts/build_container.sh $1" <<< $(cat ~/passfile)
+ssh cvuser@cv2 -t "sudo ~/mesh/k3s ctr images import ~/mesh/tmp/$1.tar" <<< $(cat ~/passfile)
+ssh cvuser@cv2 -t "sudo chmod +777 ~/mesh/tmp/$1.tar" <<< $(cat ~/passfile)
 scp cvuser@cv2:~/mesh/tmp/$1.tar ~/mesh/tmp/
 
 echo "Deleting residuals from master"
-ssh cvuser@cv2 -t "rm -rf ~/mesh/tmp/scripts" <<< $2
-ssh cvuser@cv2 -t "rm -rf ~/mesh/tmp/$1" <<< $2
-ssh cvuser@cv2 -t "rm ~/mesh/tmp/$1.tar" <<< $2
+ssh cvuser@cv2 -t "rm -rf ~/mesh/tmp/scripts" <<< $(cat ~/passfile)
+ssh cvuser@cv2 -t "rm -rf ~/mesh/tmp/$1" <<< $(cat ~/passfile)
+ssh cvuser@cv2 -t "rm ~/mesh/tmp/$1.tar" <<< $(cat ~/passfile)
 
 for i in 1
 do
     echo "Running script on cv$i"
     scp ~/mesh/tmp/$1.tar cvuser@cv$i:~/mesh/tmp/
-    ssh cvuser@cv$i -t "sudo ~/mesh/k3s ctr images import ~/mesh/tmp/$1.tar" <<< $2
-    ssh cvuser@cv$i -t "rm -rf ~/mesh/tmp/$1.tar" <<< $2
+    ssh cvuser@cv$i -t "sudo ~/mesh/k3s ctr images import ~/mesh/tmp/$1.tar" <<< $(cat ~/passfile)
+    ssh cvuser@cv$i -t "rm -rf ~/mesh/tmp/$1.tar" <<< $(cat ~/passfile)
 done
 
 rm ~/mesh/tmp/$1.tar
