b11655a8b83f1e7887d59e498a011c67da130566
Adwait Bauskar <AdwaitB@users.noreply.github.com>
2022-10-06 19:26:23 -0400

    Added redirect script for prometheus

---
+++ scripts/redirect.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import requests
+from flask import Flask,request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'], defaults={'path': ''})
+@app.route('/<path:path>', methods=['GET'])
+def hello(path):
+    forwardto = sys.argv[1] + request.full_path
+    print(forwardto, file=sys.stderr)
+    return requests.get(forwardto).text
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9091)
