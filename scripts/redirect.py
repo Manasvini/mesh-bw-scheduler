339e02f16180d89acf674ba62a49f60e556593dd
Adwait Bauskar <AdwaitB@users.noreply.github.com>
2022-10-06 19:43:40 -0400

    Fixed redirect.py for static files

--- scripts/redirect.py
+++ scripts/redirect.py
@@ -3,7 +3,7 @@ import sys
 import requests
 from flask import Flask,request
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/~')
 
 @app.route('/', methods=['GET'], defaults={'path': ''})
 @app.route('/<path:path>', methods=['GET'])
