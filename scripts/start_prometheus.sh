9b9c5a78f73e39512a6c84296b72ff97b7b236fa
Adwait Bauskar <AdwaitB@users.noreply.github.com>
2022-10-22 14:23:51 -0400

    prometheus fixup

--- scripts/start_prometheus.sh
+++ scripts/start_prometheus.sh
@@ -2,11 +2,9 @@ cd ~/mesh/mesh-bw-scheduler
 
 git pull
 
-scp -r ~/mesh/mesh-bw-scheduler/other cvuser@cv2:~/mesh/tmp/other
+scp -r ~/mesh/mesh-bw-scheduler/prometheus cvuser@cv2:~/mesh/tmp/prometheus
 
-sudo ./helm install --values ~/mesh/tmp/other/prometheus-install/values.yaml -f ~/mesh/tmp/other/prometheus-install/prometheus.yaml -n monitoring monitoring prometheus-community/kube-prometheus-stack --kubeconfig /etc/rancher/k3s/k3s.yaml
+echo `cat ~/passfile` | ssh -tt cvuser@cv2 "sudo ./helm install --values ~/mesh/tmp/prometheus/prometheus-install/values.yaml -f ~/mesh/tmp/prometheus/prometheus-install/prometheus.yaml -n monitoring monitoring prometheus-community/kube-prometheus-stack --kubeconfig /etc/rancher/k3s/k3s.yaml" 
 
-echo `cat ~/passfile` | ssh -tt cvuser@cv2 "sudo ./helm install --values ~/mesh/tmp/other/prometheus-install/values.yaml -f ~/mesh/tmp/other/prometheus-install/prometheus.yaml -n monitoring monitoring prometheus-community/kube-prometheus-stack --kubeconfig /etc/rancher/k3s/k3s.yaml" 
-
-echo `cat ~/passfile` | ssh -tt cvuser@cv2 "rm -rf ~/mesh/tmp/other" 
+echo `cat ~/passfile` | ssh -tt cvuser@cv2 "rm -rf ~/mesh/tmp/prometheus" 
 
