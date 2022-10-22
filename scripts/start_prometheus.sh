8fe10e53960137ddd890806af728b31275384d54
Adwait Bauskar <AdwaitB@users.noreply.github.com>
2022-10-22 14:24:43 -0400

    prometheus script fixup

--- scripts/start_prometheus.sh
+++ scripts/start_prometheus.sh
@@ -4,7 +4,7 @@ git pull
 
 scp -r ~/mesh/mesh-bw-scheduler/prometheus cvuser@cv2:~/mesh/tmp/prometheus
 
-echo `cat ~/passfile` | ssh -tt cvuser@cv2 "sudo ./helm install --values ~/mesh/tmp/prometheus/prometheus-install/values.yaml -f ~/mesh/tmp/prometheus/prometheus-install/prometheus.yaml -n monitoring monitoring prometheus-community/kube-prometheus-stack --kubeconfig /etc/rancher/k3s/k3s.yaml" 
+echo `cat ~/passfile` | ssh -tt cvuser@cv2 "sudo ~/mesh/helm install --values ~/mesh/tmp/prometheus/prometheus-install/values.yaml -f ~/mesh/tmp/prometheus/prometheus-install/prometheus.yaml -n monitoring monitoring prometheus-community/kube-prometheus-stack --kubeconfig /etc/rancher/k3s/k3s.yaml" 
 
 echo `cat ~/passfile` | ssh -tt cvuser@cv2 "rm -rf ~/mesh/tmp/prometheus" 
 
