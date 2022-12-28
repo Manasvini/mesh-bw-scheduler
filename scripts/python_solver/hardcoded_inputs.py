2e819990148478c3dbe5dc644b8a30388fd7b4d0
Adwait Bauskar <AdwaitB@users.noreply.github.com>
2022-12-28 13:09:41 -0500

    removed json dumps

--- scripts/python_solver/hardcoded_inputs.py
+++ scripts/python_solver/hardcoded_inputs.py
@@ -89,5 +89,5 @@ def get_edges(inp):
     ret.sort(reverse=True)
     return ret
 
-with open("sample.json", "w") as outfile:
-    json.dump(fill(topology), outfile)
+# with open("sample.json", "w") as outfile:
+#     json.dump(fill(topology), outfile)
