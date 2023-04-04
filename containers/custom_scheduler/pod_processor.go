6237a111199743f39138ab410a1fc4833d8633c6
Manasvini Sethuraman <msethuraman3@gatech.edu>
2023-04-04 19:11:21 +0000

    create pod grahs

---
+++ containers/custom_scheduler/pod_processor.go
@@ -0,0 +1,247 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+)
+
+//var processorLock = &sync.Mutex{}
+
+type PodProcessor struct {
+	unscheduledPods map[string]Pod // pod name to pod mapping
+	podLock         *sync.Mutex
+}
+
+func NewPodProcessor() *PodProcessor {
+	mu := &sync.Mutex{}
+	unscheduledPods := make(map[string]Pod, 0)
+	pp := &PodProcessor{unscheduledPods: unscheduledPods, podLock: mu}
+	logger("Created pod processor")
+	return pp
+}
+
+func (pp *PodProcessor) AddPod(pod Pod) {
+	pp.podLock.Lock()
+	pp.unscheduledPods[pod.Metadata.Name] = pod
+
+	pp.podLock.Unlock()
+	logger("Added pod " + pod.Metadata.Name + " to unscheduled pods")
+}
+
+func (pp *PodProcessor) AreAllRelatedPodsPresent(pod Pod, relationship string) bool {
+	// Dependee: for a pod, check if all its dependee pods (i.e., the pods that THIS pod depends on) are present
+	// Depender: for a pod, check if all its depender pods (i.e., all pods that depend on THIS pod) are present
+	pp.podLock.Lock()
+	podList := pp.unscheduledPods
+	pp.podLock.Unlock()
+	annotations := pod.Metadata.Annotations
+	logger(fmt.Sprintf("pod %s has %d annotations", pod.Metadata.Name, len(annotations)))
+	// format: dependee/bw/PODNAME / dependee/latency/PODNAME
+	for k, _ := range annotations {
+		if strings.Contains(k, relationship) {
+			vals := strings.Split(k, "/")
+			if len(vals) < 3 {
+				logger(fmt.Sprintf("ERROR: Incorrect annotation format for pod dependency %s", k))
+			}
+			podName := vals[2]
+			_, isPodPresent := podList[podName]
+			if !isPodPresent {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func (pp *PodProcessor) AreAllDependersPresent(pod Pod) bool {
+	return pp.AreAllRelatedPodsPresent(pod, "depender")
+}
+func (pp *PodProcessor) AreAllDependeesPresent(pod Pod) bool {
+	return pp.AreAllRelatedPodsPresent(pod, "dependee")
+}
+
+func (pp *PodProcessor) IsPodSpecComplete(pod Pod) bool {
+	return pp.AreAllDependersPresent(pod) && pp.AreAllDependeesPresent(pod)
+}
+
+// Build a directed pod dependency graph
+func (pp *PodProcessor) GetPodDependencyGraph(podList []Pod) map[string]map[string]bool {
+	podGraph := make(map[string]map[string]bool, 0)
+
+	for _, pod := range podList {
+		annotations := pod.Metadata.Annotations
+		for k, _ := range annotations {
+			vals := strings.Split(k, "/")
+			if len(vals) < 3 {
+				logger(fmt.Sprintf("ERROR: Incorrect annotation format for pod dependency %s", k))
+			}
+			relationship, podName := vals[0], vals[2]
+			if relationship == "depender" {
+				continue
+			}
+			_, exists := podGraph[pod.Metadata.Name]
+			if !exists {
+				podGraph[pod.Metadata.Name] = make(map[string]bool, 0)
+			}
+			podGraph[pod.Metadata.Name][podName] = true
+		}
+
+	}
+	return podGraph
+}
+
+// Build an undirected graph of pod dependencies from all unscheduled pods
+// A pod is added to the graph iff all its dependencies are met, and all the pods that are dependent on this pod are also in the list of unscheduled pods
+func (pp *PodProcessor) GetPodGraph() map[string]map[string]bool {
+	pp.podLock.Lock()
+	podList := pp.unscheduledPods
+	pp.podLock.Unlock()
+	podGraph := make(map[string]map[string]bool, 0)
+
+	for _, pod := range podList {
+		if !pp.IsPodSpecComplete(pod) {
+			continue
+		}
+		annotations := pod.Metadata.Annotations
+		for k, _ := range annotations {
+			vals := strings.Split(k, "/")
+			if len(vals) < 3 {
+				logger(fmt.Sprintf("ERROR: Incorrect annotation format for pod dependency %s", k))
+			}
+			podName := vals[2]
+			_, exists := podGraph[pod.Metadata.Name]
+			if !exists {
+				podGraph[pod.Metadata.Name] = make(map[string]bool, 0)
+			}
+			_, exists = podGraph[podName]
+			if !exists {
+				podGraph[podName] = make(map[string]bool, 0)
+			}
+			podGraph[pod.Metadata.Name][podName] = true
+			podGraph[podName][pod.Metadata.Name] = true
+		}
+
+	}
+	for srcName, deps := range podGraph {
+		logger("Pod: " + srcName)
+		logger("Dependers: ")
+		fmtStr := ""
+		for dstName, _ := range deps {
+			fmtStr += dstName + ", "
+		}
+		logger("Dependers: " + fmtStr)
+
+	}
+	return podGraph
+}
+
+func isInList(elem string, list []string) bool {
+	for _, e := range list {
+		if e == elem {
+			return true
+		}
+	}
+	return false
+}
+
+// Perform bfs starting from PodName
+func (pp *PodProcessor) GetPodGroup(podName string, podGraph map[string]map[string]bool) []string {
+	visited := make(map[string]bool, 0)
+	visited[podName] = true
+	queue := make([]string, 0)
+	queue = append(queue, podName)
+	for {
+		if len(queue) == 0 || queue == nil {
+			break
+		}
+		logger(fmt.Sprintf("q has %d pods", len(queue)))
+		pod := queue[0]
+		if len(queue) > 1 {
+
+			queue = queue[1:len(queue)]
+		} else {
+			queue = nil
+		}
+
+		_, exists := podGraph[pod]
+		if !exists {
+			panic("Unknown pod " + pod)
+		}
+		visited[pod] = true
+		for node, _ := range podGraph[pod] {
+			_, visited := visited[node]
+			if !visited && !isInList(node, queue) {
+				queue = append(queue, node)
+			}
+		}
+	}
+	podList := make([]string, 0)
+	for pod, v := range visited {
+		if v == true {
+			podList = append(podList, pod)
+		}
+	}
+	return podList
+}
+
+// pod group is a set of pods that need to be scheduled together. They represent an application spec
+// We perform an undirected graph traversal and keep track of the connected components in the graph
+func (pp *PodProcessor) GetPodGroups(podGraph map[string]map[string]bool) [][]string {
+	visited := make(map[string]bool, 0)
+	podGroups := make([][]string, 0)
+	for {
+		if len(visited) == len(podGraph) {
+			break
+		}
+		for pod, _ := range podGraph {
+			_, exists := visited[pod]
+			if exists {
+				continue
+			}
+			podList := pp.GetPodGroup(pod, podGraph)
+			logger(fmt.Sprintf("Got %d pods from %s ", len(podList), pod))
+			for _, p := range podList {
+				visited[p] = true
+			}
+			podGroups = append(podGroups, podList)
+		}
+	}
+	return podGroups
+}
+
+func (pp *PodProcessor) MarkScheduled(pods []Pod) {
+	pp.podLock.Lock()
+	for _, pod := range pods {
+		podName := pod.Metadata.Name
+		_, exists := pp.unscheduledPods[podName]
+		if exists {
+			delete(pp.unscheduledPods, podName)
+		}
+	}
+	pp.podLock.Unlock()
+}
+
+// return first pod group that is unscheduled
+func (pp *PodProcessor) GetUnscheduledPods() []Pod {
+	pp.podLock.Lock()
+	podList := pp.unscheduledPods
+	pp.podLock.Unlock()
+
+	unscheduled := make([]Pod, 0)
+	podGraph := pp.GetPodGraph()
+	if len(podGraph) == 0 {
+		return unscheduled
+	}
+	podGroups := pp.GetPodGroups(podGraph)
+	pods := make([]Pod, 0)
+	for _, podName := range podGroups[0] {
+		pod, exists := podList[podName]
+		if exists {
+			pods = append(pods, pod)
+		} else {
+			panic("Unknown pod " + podName)
+		}
+	}
+	return pods
+}
---
+++ containers/custom_scheduler/pod_processor_test.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"testing"
+)
+
+func getPodSimpleTopo() map[string]Pod {
+	pods := make(map[string]Pod, 0)
+
+	// p0 --> p1 --> p2
+	ann1 := map[string]string{"dependee/bw/pod_1": "1Mbps", "dependee/latency/pod_1": "10ms"}
+	ann2 := map[string]string{"dependee/bw/pod_2": "1Mbps", "dependee/latency/pod_2": "10ms", "depender/bw/pod_0": "1Mbps", "depender/latency/pod_0": "10ms"}
+	ann3 := map[string]string{"depender/bw/pod_1": "1Mbps", "depender/latency/pod_1": "10ms"}
+
+	podMeta := Metadata{Name: "pod_0", Annotations: ann1}
+	pods["pod_0"] = Pod{Kind: "pod", Metadata: podMeta}
+	podMeta = Metadata{Name: "pod_1", Annotations: ann2}
+	pods["pod_1"] = Pod{Kind: "pod", Metadata: podMeta}
+	podMeta = Metadata{Name: "pod_2", Annotations: ann3}
+	pods["pod_2"] = Pod{Kind: "pod", Metadata: podMeta}
+
+	return pods
+}
+
+func getPodDisconnectedTopo() map[string]Pod {
+	pods := make(map[string]Pod, 0)
+
+	// p0 --> p1 --> p2 p4 --> p5
+	ann1 := map[string]string{"dependee/bw/pod_1": "1Mbps", "dependee/latency/pod_1": "10ms"}
+	ann2 := map[string]string{"dependee/bw/pod_2": "1Mbps", "dependee/latency/pod_2": "10ms", "depender/bw/pod_0": "1Mbps", "depender/latency/pod_0": "10ms"}
+	ann3 := map[string]string{"depender/bw/pod_1": "1Mbps", "depender/latency/pod_1": "10ms"}
+
+	ann4 := map[string]string{"dependee/bw/pod_5": "1Mbps", "dependee/latency/pod_5": "10ms"}
+	ann5 := map[string]string{"depender/bw/pod_4": "1Mbps", "depender/latency/pod_5": "10ms"}
+
+	podMeta := Metadata{Name: "pod_0", Annotations: ann1}
+	pods["pod_0"] = Pod{Kind: "pod", Metadata: podMeta}
+	podMeta = Metadata{Name: "pod_1", Annotations: ann2}
+	pods["pod_1"] = Pod{Kind: "pod", Metadata: podMeta}
+	podMeta = Metadata{Name: "pod_2", Annotations: ann3}
+	pods["pod_2"] = Pod{Kind: "pod", Metadata: podMeta}
+	podMeta = Metadata{Name: "pod_4", Annotations: ann4}
+	pods["pod_4"] = Pod{Kind: "pod", Metadata: podMeta}
+	podMeta = Metadata{Name: "pod_5", Annotations: ann5}
+	pods["pod_5"] = Pod{Kind: "pod", Metadata: podMeta}
+
+	return pods
+}
+
+func TestGetPodGraph1(t *testing.T) {
+	pods := getPodSimpleTopo()
+	pp := NewPodProcessor()
+	pp.unscheduledPods = pods
+	podGraph := pp.GetPodGraph()
+
+	wantPodGraph := map[string]map[string]bool{"pod_0": {"pod_1": true}, "pod_1": {"pod_0": true, "pod_2": true}, "pod_2": {"pod_1": true}}
+
+	if len(wantPodGraph) != len(podGraph) {
+		t.Fatalf("Want %d got %d instead", len(wantPodGraph), len(podGraph))
+	}
+}
+
+func TestGetPodGraphComponents(t *testing.T) {
+	pods := getPodSimpleTopo()
+	pp := NewPodProcessor()
+	pp.unscheduledPods = pods
+	podGraph := pp.GetPodGraph()
+
+	podgroups := pp.GetPodGroups(podGraph)
+	if len(podgroups) != 1 {
+		t.Fatalf("Want 1 pod group, got %d instead", len(podgroups))
+	}
+}
+
+func TestGetPodGraphComponents1(t *testing.T) {
+	pods := getPodDisconnectedTopo()
+	pp := NewPodProcessor()
+	pp.unscheduledPods = pods
+	podGraph := pp.GetPodGraph()
+
+	podgroups := pp.GetPodGroups(podGraph)
+	if len(podgroups) != 2 {
+		t.Fatalf("Want 2 pod group, got %d instead", len(podgroups))
+	}
+}
+
+func TestDepGraphComponents(t *testing.T) {
+	pods := getPodSimpleTopo()
+	pp := NewPodProcessor()
+	pp.unscheduledPods = pods
+	podGraph := pp.GetPodGraph()
+
+	podgroups := pp.GetPodGroups(podGraph)
+
+	wantPodGraph := map[string]map[string]bool{"pod_0": {"pod_1": true}, "pod_1": {"pod_2": true}}
+
+	podList := make([]Pod, 0)
+	for _, podName := range podgroups[0] {
+		podList = append(podList, pods[podName])
+	}
+
+	depGraph := pp.GetPodDependencyGraph(podList)
+
+	if len(wantPodGraph) != len(depGraph) {
+		t.Fatalf("Want %d got %d instead", len(wantPodGraph), len(depGraph))
+	}
+}
