c9b0ace3ec5481eccb6aca30d29cf7a956ae1c54
Manasvini Sethuraman <msethuraman3@gatech.edu>
2023-04-25 21:14:16 +0000

    refactor client

---
+++ containers/netmon/netmon_client/net_types.go
@@ -0,0 +1,27 @@
+package netmon_client
+
+type Link struct {
+	Source      string
+	Destination string
+	Latency     float64
+	Bandwidth   float64
+}
+
+type LinkSet map[string]map[string]Link
+
+type Path struct {
+	Source      string
+	Destination string
+	Hops        []string
+	Bandwidth   float64
+	Latency     float64
+}
+type PathSet map[string]map[string]Path
+
+type Traffic struct {
+	Source      string
+	Destination string
+	Bytes       float64
+}
+
+type TrafficSet map[string]map[string]Traffic
